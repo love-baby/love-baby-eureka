d45244717c2c5522a0bfe48db373393434db0725
liangbc <bicong.liang@51zouchuqu.com>
2017-09-23 18:09:56 +0800

    初始化

--- src/main/java/com/love/baby/eureka/EurekaApplication.java
+++ src/main/java/com/love/baby/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.love.baby.eureka;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
