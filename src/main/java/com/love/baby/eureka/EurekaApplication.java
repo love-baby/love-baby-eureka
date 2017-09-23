76a79780d940d689121270c879fe337f67f15c3f
liangbc <bicong.liang@51zouchuqu.com>
2017-09-23 17:57:49 +0800

    初始化

--- src/main/java/com/love/baby/eureka/EurekaApplication.java
+++ src/main/java/com/love/baby/eureka/EurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaApplication {
     public static void main(String[] args) {
