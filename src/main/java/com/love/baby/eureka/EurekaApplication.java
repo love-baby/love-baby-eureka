9d56a19447fe8f2a0015ac5800759649cc2f2b0e
liangbc <bicong.liang@51zouchuqu.com>
2018-03-22 12:28:28 +0800

    升级 spring boot 2.0 spring cloud m8

--- src/main/java/com/love/baby/eureka/EurekaApplication.java
+++ src/main/java/com/love/baby/eureka/EurekaApplication.java
@@ -1,15 +1,13 @@
 package com.love.baby.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
-		new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
